470b57ab8941d4da6a9047b52a586893161d534b
StringCGE <Christian_eduardo12@hotmail.com>
2022-05-15 01:37:10 -0500

    olvidaba subir las tablas usadas

--- sql.sql
+++ sql.sql
@@ -9,3 +9,37 @@ ALTER TABLE prueba
 
 ALTER TABLE prueba
   MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+
+  CREATE TABLE basededatos.prospecto (
+	id_prospecto INT NOT NULL AUTO_INCREMENT,
+	cedula VARCHAR(10) NOT NULL,
+	p_nombre VARCHAR(40) NOT NULL,
+	s_nombre VARCHAR(40) NOT NULL,
+	p_apellido VARCHAR(40) NOT NULL,
+	s_apellido VARCHAR(40) NOT NULL,
+	fk_sexo INT NOT NULL,
+	fk_nacionalidad INT NOT NULL,
+	fecha_nac DATETIME NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	celular_1 VARCHAR(10) NOT NULL,
+	celular_2 VARCHAR(10) NOT NULL,
+	dir_domicilio VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id_prospecto),
+	
+	UNIQUE (cedula)
+) ENGINE = InnoDB;
+
+CREATE TABLE basededatos.sexo (
+	id_sexo INT NOT NULL AUTO_INCREMENT,
+	detalle VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id_sexo),
+	UNIQUE (detalle)
+) ENGINE = InnoDB;
+
+CREATE TABLE basededatos.nacionalidad (
+	id_nacionalidad INT NOT NULL AUTO_INCREMENT,
+	detalle VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id_nacionalidad),
+	UNIQUE (detalle)
+) ENGINE = InnoDB;
